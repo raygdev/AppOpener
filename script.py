794d6ef79a3cfa345dbff66264bbd7d129569055
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-03 08:57:02 -0400

    Add cdk window , user name and password prompts
    Add a way to open a cdk window and log in

--- script.py
+++ script.py
@@ -1,42 +1,61 @@
 import pyautogui
 import time
 
+#use sleep later to allow the other windows to load
+time.sleep(7)
+
 # Prompt for the Techline Connect User Name
-username = pyautogui.prompt("TLC User Name")
+TLCusername = pyautogui.prompt("TLC User Name")
 
 # If nothing is put in to the username prompt
-while not username:
+while not TLCusername:
     # keep prompting until a username is obtained
-    username = pyautogui.prompt("TLC User Name must be provided")
+    TLCusername = pyautogui.prompt("TLC User Name must be provided")
 
 #Prompt for Techline Connect Password
-password = pyautogui.password("TLC Password")
+TLCpassword = pyautogui.password("TLC Password")
 
 #If nothing is put in to the password prompt
-while not password:
+while not TLCpassword:
     #keep prompting until a password is obtained
-    password = pyautogui.password("A TLC password must be provided")
+    TLCpassword = pyautogui.password("A TLC password must be provided")
+
+
+
+cdkUsername = pyautogui.prompt("CDK username")
+
+cdkPassword = pyautogui.password("CDK password")
+
+while not cdkUsername:
+     cdkUsername = pyautogui.prompt("CDK username")
+
+while not cdkPassword:
+     cdkPassword = pyautogui.password("CDK password")
 
 
 # find the browser window with the proper title for the "VSP Logon Form"
 browserLogon = pyautogui.getWindowsWithTitle("VSP Logon Form")[0]
+cdkLogon = pyautogui.getWindowsWithTitle("CDK")[0]
+powershell = pyautogui.getWindowsWithTitle("PowerShell")[0]
+powershell.minimize()
+cdkLogon.minimize()
 
 #The browser window has to be minimized first when it is opened
 browserLogon.minimize()
+
+cdkLogon.maximize()
+time.sleep(5)
+pyautogui.typewrite(cdkUsername)
+pyautogui.press("tab")
+pyautogui.typewrite(cdkPassword)
+pyautogui.press("enter")
+cdkLogon.minimize()
 '''
 after minimizing the window, maximizing it will
 bring it to the foreground before typing is done
 '''
 browserLogon.maximize()
-
-#use sleep later to allow the other windows to load
-# time.sleep(1)
-
-# globalUserInputLocation = pyautogui.locateOnScreen("globaluserinput.png")
-
-print(browserLogon)
-
-# pyautogui.moveTo(globalUserInputLocation.left + 10, globalUserInputLocation.top + 30)
+time.sleep(2)
 
 '''
 The typewrite function will allow the inputs to be provided.
@@ -46,9 +65,6 @@ After the password is input, the "\n" will be the enter button.
 
 Second argument is interval (in seconds) for each character typed.
 '''
-pyautogui.typewrite(username + "\t" + password + "\n")
+pyautogui.typewrite(TLCusername + "\t" + TLCpassword + "\n")
 
 browserLogon.minimize()
-
-
-
