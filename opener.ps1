989e0812903915b7abd6c804d52d96cc7e9e342b
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-03 08:59:08 -0400

    Create opener
    Allows powershell to open the required apps. and executes py script for the automation.

---
+++ opener.ps1
@@ -0,0 +1,3 @@
+Start-Process "C:\Users\Public\Desktop\CDK Drive.lnk"
+Start-Process "https://www.autopartners.net/gmentsso/UI/Login?goto=https%3A%2F%2Fdealer.autopartners.net%3A443%2F"
+py C:\Users\Tech500\Desktop\script.py
